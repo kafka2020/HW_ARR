52f6772736130b339ba4918ad4150158be10c4e1
Andrew S <>
2025-03-19 19:47:22 +0300

    step 3 create method showMenu

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        String[] products = {"Хлеб", "Яблоки", "Молоко"};
-        int[] prices = {100, 200, 300};
+        String[] products = {"Хлеб", "Яблоки", "Молоко", "Гречка"};
+        int[] prices = {100, 200, 300, 400};
         int[] counters = new int[products.length];
 
         showMenu(products, prices);
-        dialogCustomer(counters);
-        calcResults(products, prices, counters);
+//        dialogCustomer(counters);
+//        calcResults(products, prices, counters);
+    }
+
+    private static void showMenu(String[] products, int[] prices) {
+        System.out.println("Список возможных товаров для покупки");
+        for (int i = 0; i < products.length; i++) {
+            System.out.printf("%d. %s %d руб/шт\n",
+                    i + 1,
+                    products[i],
+                    prices[i]
+            );
+            
+        }
     }
 }
